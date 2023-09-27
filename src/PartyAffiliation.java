eb2c3222cf7161e21fe7a3b001349f70da059850
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-27 09:33:59 -0400

    Part 3 of Lab 5 of Unit 5

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,5 +1,32 @@
-public class Main {
+import java.util.Scanner;
+
+public class PartyAffiliation {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String dem = "D";
+        String rep = "R";
+        String ind = "I";
+        String userParty;
+
+
+        System.out.println("What is your party affiliation? Please use only the first letter capitalized.");
+        userParty = scan.nextLine();
+
+        if (userParty.equals(dem)){
+            System.out.println("You get a Democratic Donkey.");
+        }
+        else if(userParty.equals(rep)) {
+            System.out.println("You get a Republican Elephant.");
+        }
+        else if (userParty.equals(ind)){
+            System.out.println("You get an Independent Person.");
+        }
+        else{
+            System.out.println("Sorry you have entered an invalid party. Please try again.");
+            System.exit(0);
+        }
+
     }
+
 }
